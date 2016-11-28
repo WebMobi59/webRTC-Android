67572c284f51790ec3448242dd121694eb6ad5b9
SelfnessAid <daxianjin59@hotmail.com>
2016-11-28 23:52:38 +0800

    Add Login

---
+++ app/src/main/java/mmstart0312/com/webrtc_android/ConfirmCode.java
@@ -0,0 +1,6 @@
+package mmstart0312.com.webrtc_android;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ConfirmCode extends AppCompatActivity {
+}
---
+++ app/src/main/java/mmstart0312/com/webrtc_android/MainActivity.java
@@ -0,0 +1,26 @@
+package mmstart0312.com.webrtc_android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.loginBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),ConfirmCode.class);
+                startActivity(i);
+            }
+        });
+    }
+}
