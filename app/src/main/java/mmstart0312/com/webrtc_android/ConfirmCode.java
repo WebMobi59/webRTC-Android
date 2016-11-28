379cc3c675fa3995f48ddf924f5116343224f17a
SelfnessAid <daxianjin59@hotmail.com>
2016-11-29 02:12:42 +0800

    added ConfirmCode Screen

--- app/src/main/java/mmstart0312/com/webrtc_android/ConfirmCode.java
+++
@@ -1,6 +0,0 @@
-package mmstart0312.com.webrtc_android;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class ConfirmCode extends AppCompatActivity {
-}
--- app/src/main/java/mmstart0312/com/webrtc_android/MainActivity.java
+++ app/src/main/java/mmstart0312/com/webrtc_android/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import mmstart0312.com.webrtc_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.loginBtn);
+        TextView button = (TextView) findViewById(R.id.btn_submit);
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                Intent i = new Intent(getApplicationContext(),ConfirmCode.class);
-                startActivity(i);
+                Intent intent = new Intent(getApplicationContext(),confirmCodeActivity.class);
+                startActivity(intent);
             }
         });
     }
