47745ccd064fe846e284d6fcabe4ed0ea2c534b6
SelfnessAid <daxianjin59@hotmail.com>
2016-11-29 06:17:12 +0800

    added RoomScreen

--- app/src/main/java/mmstart0312/com/webrtc_android/confirmCodeActivity.java
+++ app/src/main/java/mmstart0312/com/webrtc_android/confirmCodeActivity.java
@@ -1,7 +1,10 @@
 package mmstart0312.com.webrtc_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class confirmCodeActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class confirmCodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_code);
+        TextView button = (TextView) findViewById(R.id.btn_confirm);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(getApplicationContext(),RoomActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
