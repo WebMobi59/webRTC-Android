5bd36e4be0ef49e6c982ce837ae4fb3672b58f0d
SelfnessAid <daxianjin59@hotmail.com>
2016-12-06 10:49:45 +0800

    add APIManager

--- app/src/main/java/mmstart0312/com/webrtc_android/MainActivity.java
+++ app/src/main/java/mmstart0312/com/webrtc_android/MainActivity.java
@@ -4,10 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import mmstart0312.com.webrtc_android.R;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView button = (TextView) findViewById(R.id.btn_submit);
+        final EditText phone_number = (EditText) findViewById(R.id.edit_phone);
+        final OkHttpClientManager clientManager = new OkHttpClientManager();
+        final Map<String,String> params =  new HashMap<String,String>();
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
+
                 Intent intent = new Intent(getApplicationContext(),confirmCodeActivity.class);
                 startActivity(intent);
             }
